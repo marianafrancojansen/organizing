465d2c581669a6a3e50f944edb7b17f5fa0e4d0b
Mariana <mariana@Stefan.local>
2014-04-01 13:57:53 -0600

    settings fix db

--- organizing/settings.py
+++ organizing/settings.py
@@ -58,7 +58,7 @@ WSGI_APPLICATION = 'organizing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {'default': dj_database_url.config()}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
