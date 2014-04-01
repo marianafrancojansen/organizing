acff681c926ec3092fef5daf91c1eb9a8025cef2
Mariana <mariana@Stefan.local>
2014-04-01 13:48:12 -0600

    settings fix db

--- organizing/settings.py
+++ organizing/settings.py
@@ -58,12 +58,7 @@ WSGI_APPLICATION = 'organizing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES['default'] =  dj_database_url.config()
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
