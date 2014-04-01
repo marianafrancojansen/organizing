8b65618f20a1d0ab9a4a2fa92f5ffaac1649be11
Mariana <mariana@stefan.lan>
2014-04-01 12:51:09 -0600

    fixing static

--- organizing/wsgi.py
+++ organizing/wsgi.py
@@ -7,8 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "organizing.settings")
+# import os
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "organizing.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
