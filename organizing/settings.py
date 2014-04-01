5734bf6b5f25c0e6ca97daa428afd1b71493bba3
Mariana <mariana@stefan.lan>
2014-04-01 12:49:27 -0600

    fixing static

--- organizing/settings.py
+++ organizing/settings.py
@@ -91,4 +91,9 @@ TEMPLATE_DIRS = [join(BASE_DIR, 'templates')]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH, 'static'),
+)
